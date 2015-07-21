db5a6ed05d6a5ee555018e6b62b6f08683082946
Douézan-Grard Guillaume <silverwyrda@gmail.com>
2015-07-21 12:25:15 +0200

    Add more customizable 'server' settings

--- server.go
+++ server.go
@@ -12,24 +12,28 @@ type Server struct {
 	Port int
 }
 
-func (config Config) Server() (Server, error) {
+func (config Config) CustomServer(name, defaultHost string, defaultPort int) (Server, error) {
 	var server Server
 
-	if err := json.Unmarshal(config.Components["server"], &server); err != nil {
-		logs.Warning("%s: %s", err.Error(), "missing or wrong 'server' configuration, ignoring")
+	if err := json.Unmarshal(config.Components[name], &server); err != nil {
+		logs.Warning("%s: %s", err.Error(), "missing or wrong '"+name+"' configuration, ignoring")
 	}
 
 	if server.Host == "" {
-		server.Host = "0.0.0.0"
-		logs.Warning("missing server 'host' configuration, assuming default value: '0.0.0.0'")
+		server.Host = defaultHost
+		logs.Warning("missing " + name + " 'host' configuration, assuming default value: '" + defaultHost + "'")
 	}
 
 	if server.Port == 0 {
-		server.Port = 8080
-		logs.Warning("missing server 'port' configuration, assuming default value: 8080")
+		server.Port = defaultPort
+		logs.Warning("missing "+name+" 'port' configuration, assuming default value: %d", defaultPort)
 	}
 
 	return server, nil
+}
+
+func (config Config) Server() (Server, error) {
+	return config.CustomServer("server", "0.0.0.0", 8080)
 }
 
 func (s Server) String() string {
